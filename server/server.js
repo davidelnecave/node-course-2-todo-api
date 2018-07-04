269426101cee823af846172a5c40c890909b4901
davidelnecave <oscarelnecave@gmail.com>
2018-07-04 13:04:12 -0500

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -8,7 +9,7 @@ var {User} = require('./models/user');
 
 var app = express();
 //To deplay to HEROKU
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 //MIDDLEWARE
 app.use(bodyParser.json());
@@ -72,5 +73,30 @@ app.listen(port, () => {
 	console.log(`Started up on port ${port}`);
 });
 
+app.patch('todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if(_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	})
+});
 
 module.exports = {app};
